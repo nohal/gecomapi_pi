ccb1e2a1602052c83d98d3d7326ba44f2656ce40
Pavel Kalian <pavel@kalian.cz>
2011-04-13 20:49:39 +0200

    Fixed possible memory leak

--- src/gecomapi.cpp
+++ src/gecomapi.cpp
@@ -592,6 +592,9 @@ void GEUIDialog::SaveViewAsGpx( wxString filename, wxString viewname )
       //TODO: read
       gpxroot->AddWaypoint(new GpxWptElement(GPX_WPT_WAYPOINT, lat, lon, 0, &wxDateTime::Now(), 0, -1, _T("Center"), wxEmptyString, viewname, wxEmptyString, NULL, _T("triangle"), _T("WPT")));
       gpx->SaveFile(filename);
+      gpxroot->Clear();
+      delete gpxroot;
+      delete gpx;
 }
 
 void GEUIDialog::SaveViewAsJPG( wxString filename ) 
