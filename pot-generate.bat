1ec8d86eb1d2093bc759af8b32ec63651aa128f0
Pavel Kalian <pavel@kalian.cz>
2012-03-15 22:28:46 +0100

    TLB added

--- pot-generate.bat
+++
@@ -1,1 +0,0 @@
-xgettext.exe --force-po --output=C:/WORK/opencpn/source/opencpn/plugins/gecomapi_pi/po/gecomapi_pi.pot --keyword=_ --width=80 --files-from=C:/WORK/opencpn/source/opencpn/plugins/gecomapi_pi/po/POTFILES.in
--- src/icons.bat
+++
@@ -1,1 +0,0 @@
-png2wx.pl -C icons.cpp -H icons.h -M ICONS_H gecomapi.png gecomapi_pi.png
